1aa430581035fff91c55f73831ac7cfc73b0f1e2
Brandon Ainge <10888736@uvu.edu>
2022-07-16 20:21:38 -0600

    Completed challenge

--- UFO Defence Force Game/Assets/Scripts/DetectCollision.cs
+++ UFO Defence Force Game/Assets/Scripts/DetectCollision.cs
@@ -7,6 +7,6 @@ public class DetectCollision : MonoBehaviour
   void OnTriggerEnter(Collider other)
   {
 	  Destroy(gameObject); // destroy this game object
-	  Destroy(other.gameObject); // destroys the other game object it hits
+	 // Destroy(other.gameObject); // destroys the other game object it hits
   }
 }
